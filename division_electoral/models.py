436193754c698b8ae1ec42bd3e00ef31b4fdc532
Felipe Álvarez <falvarez@votainteligente.cl>
2012-01-03 14:44:20 -0300

    wip

--- division_electoral/models.py
+++ division_electoral/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Circunscripcion(models.Model):
-	district = models.ForeignKey('District')
 	name = models.CharField(max_length = 200)
 
 class District(models.Model):
 	name = models.CharField(max_length = 200)
+        circunscripcion = models.ForeignKey(Circunscripcion, related_name="District" )
 
 class Comuna(models.Model):
 	region = models.ForeignKey('Region')
--- division_electoral/tests.py
+++ division_electoral/tests.py
@@ -14,18 +14,19 @@ from division_electoral.models import Region
 class CircuscripcionTest(TestCase):
 	def test_create_circunscripcion(self):
 		#create circunscripcion from scratch
-		district, created = District.objects.get_or_create(name='district 1')
-		circunscripcion, created = Circunscripcion.objects.get_or_create(name='circunscripcion 1',district = district)
+		circunscripcion, created = Circunscripcion.objects.get_or_create(name='circunscripcion 1')
 		self.assertTrue(created)
 		self.assertTrue(circunscripcion.name=='circunscripcion 1')
-		self.assertTrue(circunscripcion.district.name=='district 1')
 	
 class DistrictTest(TestCase):
 	def test_create_district(self):
 		#create district from scratch
-		district, created = District.objects.get_or_create(name='district 1')
-		self.assertTrue(created)
+                circunscripcion = Circunscripcion.objects.create(name='circunscripcion 1')
+		district = District.objects.create(name='district 1',circunscripcion = circunscripcion)
+		self.assertIsNotNone(district.pk)
 		self.assertTrue(district.name=='district 1')
+                self.assertEqual(district.circunscripcion, circunscripcion)
+
 
 class RegionTest(TestCase):
 	def test_create_region(self):
@@ -37,7 +38,8 @@ class RegionTest(TestCase):
 class ComunaTest(TestCase):
 	def test_create_comuna(self):
 		#create comuna from scratch
-		district, created = District.objects.get_or_create(name='district 1')
+                circunscripcion = Circunscripcion.objects.create(name='circunscripcion 1')
+		district, created = District.objects.get_or_create(name='district 1', circunscripcion = circunscripcion)
 		region, created = Region.objects.get_or_create(name='region 1')
 		comuna, created = Comuna.objects.get_or_create(name='comuna 1', region = region, district = district)
 		self.assertTrue(created)
