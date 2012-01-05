c02c4d8ee7b93aebf8a5cf98b87cb00b46314b84
Pedro Daire <pdaire@gmail.com>
2012-01-04 23:14:05 -0300

    Adds the name in all electoral areas

--- electoralarea/models.py
+++ electoralarea/models.py
@@ -3,25 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Circunscripcion(models.Model):
-	# name = models.CharField(max_length = 200)
+	name = models.CharField(max_length = 200)
 	def __unicode__(self):
 		return self.name
 
 class District(models.Model):
 	circunscripcion = models.ForeignKey('Circunscripcion')
-	# name = models.CharField(max_length = 200)
+	name = models.CharField(max_length = 200)
 	def __unicode__(self):
 		return self.name
 
 class Comuna(models.Model):
 	region = models.ForeignKey('Region')
 	district = models.ForeignKey('District')
-	# name = models.CharField(max_length = 200)
+	name = models.CharField(max_length = 200)
 	def __unicode__(self):
 		return self.name
 
 class Region(models.Model):
-	# name = models.CharField(max_length = 200)
+	name = models.CharField(max_length = 200)
 	def __unicode__(self):
 		return self.name
 	
